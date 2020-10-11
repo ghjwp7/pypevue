19dfb40b0e14f41306ae8de08da50895e91d8cb9
James Waldby <j13@pat7.com>
2020-10-10 23:01:43 -0600

    with eg-objfile2 & objReader2.py working ok for method objReadFile; other methods not written yet & other code in objReader2 just junk

--- src/pypevue/examples/objReader2.py
+++ src/pypevue/examples/objReader2.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python3
 # -*- mode: python -*-
 
 # objReader.py, a plugin to read point and face data from a Wavefront
 # OBJ file # (a 3D-structures file format) - jiw 9 Oct 2020 Ref:
 # <https://www.fileformat.info/format/wavefrontobj/egff.htm>
 
-# This program defines a data structure (ObjData) and several methods
-# (objReadFile, objManyPoly, objOnePoly, objFatPoly) that load or
-# process data into or from an ObjData item.
+# This program defines a data structure class (ObjFileData) and
+# several methods (objReadFile, objManyPoly, objOnePoly, objFatPoly)
+# that load or process data into or from an ObjData item.
 
 # Usage: To incorporate this code into pypevue, in command line
 # parameters or in an `=P` line of a script, say
@@ -26,54 +27,77 @@
 # following items to green.  Not all material tags are recognized,
 # which can result in arbitrary color settings.
 
-# Method: This replaces the `hookBack()` method (a `pass`) with code
-# that reads data from a specified .obj file.  It keeps vertex and
-# face data locally (rather than adding it into pypevu's post or
-# cylinder data structures).  Then it produces code for one 3D
-# polyhedron per face read in.  Let F stand for the polygonal shape
-# specified by a certain f line from the file.  F forms one surface of
-# the polyhedron.  An opposite surface is congruent to F, offset by a
-# thickness t.  The direction of offset is (v1 x v2) where v1 and v2
-# are the first two edges of F.  Thickness t is a fraction h of RMS
-# edge length, over all faces in the file.  For example, if there are
-# 28 edges of specified faces and the total of squared lengths is
-# 23548 units, t = sqrt(23548/28)*h = 29*h = 1.45.  The default value
-# of h is 1/20.  Note, shared edges count multiple times.
+# Method: This has a method to read data from a specified .obj file.
+# It keeps vertex and face data in an instance of ObjFileData.  Other
+# methods will produce code for various things like one 3D polyhedron
+# per face read in, or treating all the faces as one polyhedron, or
+# generating a thin 3D structure for each face.
+
+# Let F stand for the polygonal shape specified by a certain f line
+# from the file.  F forms one surface of the polyhedron.  An opposite
+# surface is congruent to F, offset by a thickness t.  The direction
+# of offset is (v1 x v2) where v1 and v2 are the first two edges of F.
+# Thickness t is a fraction h of RMS edge length, over all faces in
+# the file.  For example, if there are 28 edges of specified faces and
+# the total of squared lengths is 23548 units, t = sqrt(23548/28)*h =
+# 29*h = 1.45.  The default value of h is 1/20.  Note, shared edges
+# count multiple times.
 
 from math import sqrt, pi, cos, sin, asin, atan2, degrees
 from pypevue import ssq, sssq, rotate2, isTrue, Point
 from pypevue import FunctionList as ref
 from re import sub
 #---------------------------------------------------------
-def hookBack(fout):
-    # Is a file name given?
-    fn = ref.userPar2 if ref.userPar2 else 'box.obj'
-    fn = sub('^"|"$', '', fn)   # Strip outer quotes if any
-    verts = []
-    faces = []
+class ObjFileData:
+    def __init__(self):
+        #print ('ObjFileData - _init__ - in')
+        self.fileName = None
+        self.verts = []         # Vertex list
+        self.faces = []         # Face list
+        self.stuff = []         # usemtl data, (uname, v#) pairs
+        self.group = []         # group data,  (gname, v#) pairs
+        self.linesIn  = 0       # count of lines read from file
+        self.nDrops = 0         # count of non-comment dropped lines
+        self.drops = ''         # string of dropped lines
+        #print (f'ObjFileData - _init__ - self.drops={self.drops}')
+
+#---------------------------------------------------------
+def objReadFile(fn, scalefac):
+    #  Strip outer quotes, if any, from the .obj file name
+    fn = sub('^"|"$', '', fn)
+    print (f'objReadFile says fn is {fn} and sf is {scalefac}')
+    result = ObjFileData()
+    #print (f'objReadFile - type(result)={type(result)}   result={result}')
+    result.fileName = fn
     with open(fn) as fin:
         lin = 0; skipped = []
         while (token := fin.readline()):
             lin += 1
             if token.startswith('v '):    # vertex command
-                x,y,z = [float(u) for u in token[2:].split()]
-                verts.append(Point(x, y, z))    
+                p = Point(*[float(u) for u in token[2:].split()])
+                p.scale(scalefac)
+                result.verts.append(p)    
             elif token.startswith('f '):    # face command
                 f = []
                 token = sub('/[/0-9]*', '', token)
                 for u in token[2:].split():
                     f.append(int(u)-1) # Make list of corners of face
-                faces.append(f)
+                result.faces.append(f)
             
             elif token.startswith('#') or token=='' or token=='\n':
                 pass # ignore comments and empty lines
             else:
-                #print (f"Skipping line {lin} of OBJ file: {token}")
-                if len(skipped) < 34:
-                    skipped.append(lin)
-    print (f"Skipped lines {' '.join(str(i) for i in skipped)}... of OBJ file")
+                skipped.append(lin) # Add lin to list of dropped lines
+    result.linesIn = lin
+    result.nDrops  = len(skipped)
+    result.drops   = ' '.join(skipped)
+    print (f"Obtained {len(result.verts)} vertices and {len(result.faces)} faces from {result.linesIn} lines in file {result.fileName}")
+    print (f"Skipped {result.nDrops} lines (#{result.drops[:40]}...) of the {result.linesIn} lines in file")
+
+    return result
+
+def morestuff():    
     nverts = len(verts)
-    
     # Scale the vertices and get total of edge lengths
     for i in range(nverts):
         verts[i].scale(ref.SF)
@@ -91,6 +115,7 @@ def hookBack(fout):
     thik = sqrt(totalssq/nsides)*hfactor
     print (f'nsides={nsides}   totalssq={totalssq}   hfactor={hfactor}   thik={thik}')
 
+def hookBack(fout):
     # Write OpenSCAD code for faces
     for f in faces:
         v0, v1, v2 = verts[0], verts[1], verts[2]
@@ -107,6 +132,13 @@ def hookBack(fout):
         #fout.write(f'[0, 1, {nv+1}, {nv}] ] );\n')
         fout.write(f'faces=[[{pl}]] );\n')
 
+    print (f"Skipped lines {' '.join(str(i) for i in skipped)}... of OBJ file")
+    
 #---------------------------------------------------------
-def tell(): return (hookBack,)
+def utry(fn, sf):
+    print (f'utry says fn is {fn} and sf is {sf}')
 #---------------------------------------------------------
+def tell(): return (objReadFile,utry)
+#---------------------------------------------------------
+if __name__ == '__main__':
+    objReadFile('box.obj', 1)
